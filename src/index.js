9674d4a23b4ff96b73e14a2fc9201d61f5fcc4d9
Rustam Faradjev <rystam9614@gmail.com>
2019-02-24 01:50:53 +0300

    add code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+ 
+let count=Infinity;
+let bases=[];
+for (j=2;j<base;){
+    if(base%j==0){
+      bases.push(j);
+      base/=j;
+    }
+    else {
+       j++;
+    }
+}
+
+bases.push(base);
+let length=bases.length;
+do{
+    newBase=bases[length-1];
+    let counter=0;
+        for(i=0;i<length;i++){
+          if(bases[i]==newBase){
+            counter++;
+            bases.splice(i,1);
+            length--;
+            i--;
+          }
+        }
+    let n = number;
+    let zeros=0;
+        while (Math.floor(n)>0){
+          n /=newBase;
+          zeros+=Math.floor(n);
+        }
+    zeros/=counter;
+        if (zeros<count){
+          count=Math.floor(zeros);
+        }
+ }
+  while(length>0);
+
+return count;
 }
